f8ec8ad7428a280f2ef331377a772a53ce1e34b1
Rubén Espadas Vargas <rubenespadas@rooting.es>
2012-10-04 18:38:21 +0200

    Fixed settings.py.

--- Dionaea/settings.py
+++ Dionaea/settings.py
@@ -70,7 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/Dionaea/static/",
+    "/static/",
 )
 
 # List of finder classes that know how to find static files in
@@ -119,7 +119,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/Dionaea/Templates",
+    "/Templates",
 )
 
 INSTALLED_APPS = (

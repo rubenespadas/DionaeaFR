31860fac1a95f10e5d1419c4654aff7ccb86771e
Rubén Espadas Vargas <rubenespadas@rooting.es>
2012-12-19 16:38:07 +0100

    Change Malware Unknown to Malware Unique.

--- Web/views/index.py
+++ Web/views/index.py
@@ -9,18 +9,17 @@ def index(request):
 	num_ips = Connection.objects.values('remote_host').annotate(num=Count('remote_host')).count()
 	num_urls = Download.objects.values('download_url').annotate(num=Count('download_url')).count()
 	num_downloads = Download.objects.all().count()
+	num_unique = Virustotalscan.objects.filter(virustotalscan_scanner='Sophos').count()
 	known = Virustotalscan.objects.filter(virustotalscan_scanner='Sophos').exclude(virustotalscan_result__isnull=True)
 	num_known = known.count()
-	unknown = Virustotalscan.objects.filter(virustotalscan_scanner='Sophos').exclude(virustotalscan_result__isnull=False)
-	num_unkown = unknown.count()
 	return render_to_response('index/index.html',
 		{
 			'num_connections': num_connections,
 			'num_ips': num_ips,
 			'num_urls': num_urls,
 			'num_downloads': num_downloads,
-			'num_known': num_known,
-			'num_unkown': num_unkown
+			'num_unique': num_unique,
+			'num_known': num_known
 		}
 	)
 

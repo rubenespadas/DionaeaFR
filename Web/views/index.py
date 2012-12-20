69b2c910e94bcf9ff0e53fec7ff1ee98c7582bb5
Rubén Espadas Vargas <rubenespadas@rooting.es>
2012-12-20 11:40:36 +0100

    Add malware analized in home panel.

--- Web/views/index.py
+++ Web/views/index.py
@@ -10,7 +10,7 @@ def index(request):
 	num_ips = Connection.objects.values('remote_host').annotate(num=Count('remote_host')).count()
 	num_urls = Download.objects.values('download_url').annotate(num=Count('download_url')).count()
 	num_downloads = Download.objects.all().count()
-	num_unique = Virustotalscan.objects.filter(virustotalscan_scanner=settings.ANTIVIRUS_VIRUSTOTAL).count()
+	num_analized = Virustotalscan.objects.filter(virustotalscan_scanner=settings.ANTIVIRUS_VIRUSTOTAL).count()
 	known = Virustotalscan.objects.filter(virustotalscan_scanner=settings.ANTIVIRUS_VIRUSTOTAL).exclude(virustotalscan_result__isnull=True)
 	num_known = known.count()
 	return render_to_response('index/index.html',
@@ -19,7 +19,7 @@ def index(request):
 			'num_ips': num_ips,
 			'num_urls': num_urls,
 			'num_downloads': num_downloads,
-			'num_unique': num_unique,
+			'num_analized': num_analized,
 			'num_known': num_known
 		}
 	)

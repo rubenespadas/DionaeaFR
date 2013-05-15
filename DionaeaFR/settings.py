07e83b23b0a6b681b1bf41abedb9223f588c53f4
Rubén Espadas Vargas <rubenespadas@rooting.es>
2013-05-15 13:30:41 +0200

    Fixed database path.

--- DionaeaFR/settings.py
+++ DionaeaFR/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'D:\Proyectos\Python\Django\DionaeaFR\logsql.sqlite', # Or path to database file if using sqlite3.
+        'NAME': '/opt/dionaea/var/dionaea/logsql.sqlite', # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
